89c88a1e8a74a6edc29dd8b333ef749d405ad42a
Ahti Komu <ahti.komu@gmail.com>
2014-09-30 02:21:55 +0300

    Comment out test methods for github

--- siteapp/views.py
+++ siteapp/views.py
@@ -8,13 +8,13 @@ from models import Skill, SkillLevelType, Category
 def site_index(request):
     return HttpResponse("<h2>Laborbook</h2>Implement your own views!<br>Look at the source code.")
 
-def test_view(request, some_id):
-    return HttpResponse("This id: %s" % str(some_id))
-
-def testing(request):
-    sk = Skill.objects.filter(skill_level_type=SkillLevelType.objects.all()[0])[1]
-    return HttpResponse("Testing skill: <br><br>%s " % sk.getSkillHTML())
-
-def testing2(request):
-    category = Category.objects.all()[1]
-    return HttpResponse("Testing category: <br><br>%s " % category.getCategoryHTML())
+# def test_view(request, some_id):
+#     return HttpResponse("This id: %s" % str(some_id))
+#
+# def testing(request):
+#     sk = Skill.objects.filter(skill_level_type=SkillLevelType.objects.all()[0])[1]
+#     return HttpResponse("Testing skill: <br><br>%s " % sk.getSkillHTML())
+#
+# def testing2(request):
+#     category = Category.objects.all()[1]
+#     return HttpResponse("Testing category: <br><br>%s " % category.getCategoryHTML())
